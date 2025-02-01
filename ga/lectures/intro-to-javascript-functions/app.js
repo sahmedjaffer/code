e105584da1d3fd26e3640a600c573b6ef40a6738
sahmedjaffer <s.ahmedjaffer@gmail.com>
2025-02-01 14:57:09 +0300

    add comints

--- ga/lectures/intro-to-javascript-functions/app.js
+++ ga/lectures/intro-to-javascript-functions/app.js
@@ -6,9 +6,11 @@ console.log(planetHasWater('Ploto'));
 // the created function to return the needed value
 function planetHasWater(planet){
     if (planet === 'Earth' || planet === 'Mars'){
+        //return the true value
         return true;
     }
     else {
+        //return the false value
        return false;
     }
 }
