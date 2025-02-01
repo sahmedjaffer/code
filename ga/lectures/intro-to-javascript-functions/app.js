2c67e743cd2456dcb0352b4558aad1982c150583
sahmedjaffer <s.ahmedjaffer@gmail.com>
2025-02-01 14:49:19 +0300

    add commints

--- ga/lectures/intro-to-javascript-functions/app.js
+++ ga/lectures/intro-to-javascript-functions/app.js
@@ -1,5 +1,9 @@
+
+//Clarify the variable
 const planet="";
+// Call the function and send the value to it then printout the returned value
 console.log(planetHasWater('Ploto'));
+// the created function to return the needed value
 function planetHasWater(planet){
     if (planet === 'Earth' || planet === 'Mars'){
         return true;
