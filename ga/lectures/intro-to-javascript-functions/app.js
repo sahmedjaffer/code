e8c6a7d1518b88b469edc39aa8fd9203c72ef1a4
sahmedjaffer <s.ahmedjaffer@gmail.com>
2025-02-01 15:09:24 +0300

    solve the second exercise

--- ga/lectures/intro-to-javascript-functions/app.js
+++ ga/lectures/intro-to-javascript-functions/app.js
@@ -16,3 +16,14 @@ function planetHasWater(planet){
 }
 
 
+
+const width=0;
+const height=0;
+console.log(computeArea(5, 25));
+function computeArea (width,height){
+    const totalArea = width*height;
+    const message =`The area of a rectangle with a width of ${width} and a height of ${height} is ${totalArea} square units.`;
+    return message;
+}
+
+
