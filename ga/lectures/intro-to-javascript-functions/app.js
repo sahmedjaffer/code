c13266a95206941fc1812aa94b3e0fd333c241cd
sahmedjaffer <s.ahmedjaffer@gmail.com>
2025-02-01 14:46:22 +0300

    first exercise in functions

---
+++ ga/lectures/intro-to-javascript-functions/app.js
@@ -0,0 +1,12 @@
+const planet="";
+console.log(planetHasWater('Ploto'));
+function planetHasWater(planet){
+    if (planet === 'Earth' || planet === 'Mars'){
+        return true;
+    }
+    else {
+       return false;
+    }
+}
+
+
