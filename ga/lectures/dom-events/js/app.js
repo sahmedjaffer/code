9a91141eeed328099f13d5bb810a7eac54a92fef
sahmedjaffer <s.ahmedjaffer@gmail.com>
2025-02-15 10:27:07 +0300

    update

--- ga/lectures/dom-events/js/app.js
+++ ga/lectures/dom-events/js/app.js
@@ -1,5 +1,5 @@
 const likeButtonElement = document.querySelector('#like-button');
-
+const dislikeButtonElement = document.querySelector('#dislike-button');
 // let's log it to confirm
 console.dir(likeButtonElement);
 
@@ -8,35 +8,40 @@ const commentButtonElement = document.querySelector('#comment-button');
 // let's log it to confirm
 console.dir(commentButtonElement);
 
-
-
 const commentListElement = document.querySelector('ul');
 console.dir(commentListElement);
 
 const inputElement= document.querySelector('input');
+let likesCount = 0;
+let dislikesCount = 0; 
 
+const handleReaction = (event) => {
+  if (event.target.id === 'like-button'){
+  likesCount += 1;
+  likeButtonElement.textContent=`${likesCount} like(s). like this post!`;
+}else {
+  dislikesCount+=1;
+  dislikeButtonElement.textContent=`${dislikesCount} dislike(s). Dislike this post!`;
 
-likeButtonElement.addEventListener('click', () => {
-    console.log('You clicked me!');
-  });
+}
+}
+
+likeButtonElement.addEventListener('click', handleReaction);
+
+dislikeButtonElement.addEventListener('click', handleReaction)
   
-  commentButtonElement.addEventListener('click', () => {
+commentButtonElement.addEventListener('click', () => {
     console.log("I work!");
   });
-
-  commentButtonElement.addEventListener('click', () => {
-    const commentElement = document.createElement('li');
-    commentElement.textContent = inputElement.value; // replace placeholder text
-    commentListElement.appendChild(commentElement);
-  });
-  
     
-  commentButtonElement.addEventListener('click', () => {
-    if (inputElement!=''){
+commentButtonElement.addEventListener('click', () => {
+    if (inputElement.value!==""){
     const commentElement = document.createElement('li');
-    commentElement.textContent = 'Can you hear me?';
+    commentElement.textContent = inputElement.value;
     commentListElement.appendChild(commentElement);
     }
   });
+
+
   
   
